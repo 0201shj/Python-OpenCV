c23ba60fe7aa81578c172589ac979493e19a5386
0201shj <77259440+0201shj@users.noreply.github.com>
2021-09-17 23:10:49 +0900

    Add files via upload

---
+++ Chapter07/0714.py
@@ -0,0 +1,27 @@
+#0714.py
+import cv2
+import numpy as np
+
+#1
+src = cv2.imread('./data/hand.jpg')
+hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+
+data = src.reshape((-1, 3)).astype(np.float32)
+
+#2
+K=2
+term_crit = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+ret, labels, centers = cv2.kmeans(data, K, None, term_crit, 5, cv2.KMEANS_RANDOM_CENTERS)
+
+print('centers.shape=', centers.shape)
+print('labels.shape=', labels.shape)
+print('ret=', ret)
+
+#3
+centers = np.uint8(centers)
+res = centers[labels.flatten()]
+dst = res.reshape((src.shape))
+
+cv2.imshow('dst', dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
